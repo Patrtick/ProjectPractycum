194c40eeda3b25feecdefd3913acbf806adc6b46
Patrtick <mak-007@bk.ru>
2026-04-27 22:42:03 +0500

    ,

--- backend/main.py
+++ backend/main.py
@@ -26,7 +26,9 @@ app = FastAPI()
 # Разрешаем все источники (для разработки)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://2.26.8.117:8080"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
